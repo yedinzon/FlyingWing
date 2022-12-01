76fa6adf6a22339931ce73ff8479e7528b2a1489
yedinzon <yedinzon@gmail.com>
2022-12-01 12:30:00 -0500

    Superficie de Control

---
+++ FlyingWing/Controller/Miscelanea/SuperficieControl.py
@@ -0,0 +1,16 @@
+class SuperficieControl(object):
+
+    def S_Control(s,mv):
+        if mv == 0:
+            print(s/8)
+            #r.set(s/8)
+        elif mv == 1:
+            print(s/10)
+            #r.set(s/10)
+        elif mv == 2:
+            print(s/12)
+            #r.set(s/12)
+
+
+
+
--- FlyingWing/View/VentanaPrincipal.py
+++ FlyingWing/View/VentanaPrincipal.py
@@ -1,13 +1,40 @@
 import tkinter as tk
 from View.Pizarra.Pizarra import Pizarra
 from Controller.MotorController import MotorController
+from Controller.Miscelanea.SuperficieControl import SuperficieControl
 
 class VentanaPrincipal(object):
     
     ventana = tk.Tk()
     ventana.title('Class')    
     canvas = tk.Canvas(ventana, width=400, height=400)
-    canvas.grid(row=0, columnspan=6, pady=20)    
+    canvas.grid(row=0, columnspan=6, pady=20)   
+    
+    frame=tk.Frame(ventana,width=1000,height=1000)
+    canvas.pack()
+    frame.pack()
+
+    S = tk.Entry(frame)
+    
+    S.grid(row=0,column=2,padx=15,pady=15)
+    Slabel = tk.Label(frame,text='Superficie Alar')
+    Slabel.grid(row=0,column=0,padx=15,pady=15)
+    mvenergico = tk.Button(frame,text='Energico',width=10,height=2,command=SuperficieControl.S_Control(800,0))
+    mvenergico.grid(row=1,column=1,padx=15,pady=15)
+    mvnormal = tk.Button(frame,text='Normal',width=10,height=2,command=lambda:S_Control(float(S.get()),1))
+    mvnormal.grid(row=1,column=2,padx=15,pady=15)
+    mvsuave = tk.Button(frame,text='Suave',width=10,height=2,command=lambda:S_Control(float(S.get()),2))
+    mvsuave.grid(row=1,column=3,padx=15,pady=15)
+    mvlabel = tk.Label(frame,text='Modo de Vuelo')
+    mvlabel.grid(row=1,column=0,padx=15,pady=15)
+
+    r = tk.DoubleVar()
+
+    resultadolabel = tk.Label(frame,text='La Superficie de Control es:')
+    resultadolabel.grid(row=2,column=1,padx=15,pady=15)
+    resultado = tk.Entry(frame,textvariable=r,state='disable')
+    resultado.config(justify='center')
+    resultado.grid(row=2,column=2,padx=15,pady=15)
 
     menuBar = tk.Menu(ventana)
     ventana.config(menu=menuBar)
